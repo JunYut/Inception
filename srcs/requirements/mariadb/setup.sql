13f3046bed36e82147ba0daa7e504554da5cdd07
JunYut <tanjunyu8888@gmail.com>
2025-07-16 16:24:54 +0800

    feat: now creates wordpress_db in mariadb container upon startup

--- srcs/requirements/mariadb/setup.sql
+++ srcs/requirements/mariadb/setup.sql
@@ -1,4 +1,11 @@
-CREATE DATABASE wordpress_db;
+-- Create WordPress database
+CREATE DATABASE IF NOT EXISTS wordpress_db;
 
-CREATE USER 'wp_user'@'%' IDENTIFIED BY 'securepassword';
+-- Create WordPress user with secure password
+CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY 'pass';
+
+-- Grant privileges to WordPress user
 GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wp_user'@'%';
+
+-- Reload privilege tables
+FLUSH PRIVILEGES;
