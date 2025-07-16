2f6fd2d8698a0588f769bbd9bc4543aeecb6df5f
JunYut <tanjunyu8888@gmail.com>
2025-07-16 23:56:15 +0800

    feat: now wordpress service and connect to mariadb database
    solution: set the bind-address value to 0.0.0.0 instead of default value 127.0.0.1

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -2,18 +2,27 @@
 
 #* THIS SCRIPT SHOULD ONLY BE RUN USING DOCKERFILE
 
-# Start MariaDB in background
+# Start MariaDB normally (without skip-grant-tables)
 mysqld_safe --user=mysql &
 
 # Wait for MariaDB to start
-sleep 5
+sleep 15
 
 # Execute setup script if it exists and hasn't been run
 if [ -f '/docker-entrypoint-initdb.d/setup.sql' ] && [ ! -f "/var/lib/mysql/.setup_done" ]; then
     echo 'Running setup.sql...'
-    mysql -u mysql < /docker-entrypoint-initdb.d/setup.sql
-    touch /var/lib/mysql/.setup_done
+
+    # Connect with no username/password (default for fresh MariaDB install)
+    mysql < /docker-entrypoint-initdb.d/setup.sql
+
+    if [ $? -eq 0 ]; then
+        echo 'Setup completed successfully'
+        touch /var/lib/mysql/.setup_done
+    else
+        echo 'Setup failed'
+        exit 1
+    fi
 fi
 
-# Keep the script running
+# Keep the container running
 wait
